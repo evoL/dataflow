80ffbaaf01ea88b7db08cf6dee4ef53c67dff678
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-11-30 23:49:38 +0100

    Update Module.h

--- gui/Module.h
+++ gui/Module.h
@@ -1,23 +1,21 @@
 #ifndef MODULE_H
 #define MODULE_H
-
 #include <QString>
 #include <QSize>
-
+#include <QVector>
 class Module
 {
 public:
-    Module(QString name, QSize size);
-
-    QString toString() const { return name; }
-    QSize size() const { return blockSize; }
-    void setName(QString newName) { name = newName; }
-
+Module(QString name, QSize size, QVector<QString> id_in, QVector<QString> id_out);
+QString toString() const { return name; }
+QSize size() const { return blockSize; }
+void setName(QString newName) { name = newName; }
+Module();
+QVector<QString> In;
+QVector<QString> Out;
+QString name;
 private:
-    Module();
+QSize blockSize;
 
-    QString name;
-    QSize blockSize;
 };
-
 #endif // MODULE_H
