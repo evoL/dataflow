29c13f242a2dc4afcd1c170b3c08ad6c62086fa9
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-11-30 23:48:39 +0100

    Update Module.cpp

--- gui/Module.cpp
+++ gui/Module.cpp
@@ -1,12 +1,11 @@
 #include "Module.h"
-
-Module::Module(QString name, QSize size)
+Module::Module(QString name, QSize size, QVector <QString> id_in, QVector<QString> id_out)
 {
-    this->name = name;
-    this->blockSize = size;
+this->name = name;
+this->blockSize = size;
+this->In=id_in;
+this->Out=id_out;
 }
-
 Module::Module()
 {
-
 }
