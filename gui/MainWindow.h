1ee311a1e54469e3a30481256d7f1187d37cd34c
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-11-30 23:47:57 +0100

    Update MainWindow.h

--- gui/MainWindow.h
+++ gui/MainWindow.h
@@ -1,13 +1,9 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
-
 #include "DiagramModuleItem.h"
 #include "ModulesListModel.h"
-
 #include <QMainWindow>
-
 class DiagramScene;
-
 QT_BEGIN_NAMESPACE
 class QAction;
 class QToolBox;
@@ -20,46 +16,35 @@ class QGraphicsView;
 class QStringListModel;
 class QListView;
 QT_END_NAMESPACE
-
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
-
+Q_OBJECT
 public:
-    MainWindow();
-
+MainWindow();
 private slots:
-    void modulesViewClicked();
-    void deleteItem();
-    void pointerGroupClicked(int id);
-    void itemInserted();
-    void about();
-
+void modulesViewClicked();
+void deleteItem();
+void pointerGroupClicked(int id);
+void itemInserted();
+void about();
 private:
-    void createModulesList();
-    void createActions();
-    void createMenus();
-    void createToolbars();
-
-    DiagramScene * scene;
-    QGraphicsView * view;
-
-    QAction * exitAction;
-    QAction * deleteAction;
-    QAction * aboutAction;
-
-    QMenu * fileMenu;
-    QMenu * itemMenu;
-    QMenu * aboutMenu;
-
-    QToolBar * editToolBar;
-    QToolBar * pointerToolbar;
-
-    ModulesListModel * modulesListModel;
-    QListView * modulesView;
-
-    QButtonGroup * pointerTypeGroup;
-    QAction * lineAction;
+void createModulesList();
+void createActions();
+void createMenus();
+void createToolbars();
+DiagramScene *scene;
+QGraphicsView *view;
+QAction *exitAction;
+QAction *deleteAction;
+QAction *aboutAction;
+QMenu *fileMenu;
+QMenu *itemMenu;
+QMenu *aboutMenu;
+QToolBar *editToolBar;
+QToolBar *pointerToolbar;
+ModulesListModel *modulesListModel;
+QListView *modulesView;
+QButtonGroup *pointerTypeGroup;
+QAction *lineAction;
 };
-
 #endif // MAINWINDOW_H
