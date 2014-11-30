e15a8bbae85ba98a75abdc590a9b8a2b85a21e07
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-11-30 23:45:08 +0100

    Update DiagramScene.h

--- gui/DiagramScene.h
+++ gui/DiagramScene.h
@@ -1,12 +1,11 @@
 #ifndef DIAGRAMSCENE_H
 #define DIAGRAMSCENE_H
-
 #include "DiagramModuleItem.h"
+#include "modulein.h"
+#include "moduleout.h"
 #include "ModulesListModel.h"
-
 #include <QGraphicsScene>
 #include <QListView>
-
 QT_BEGIN_NAMESPACE
 class QGraphicsSceneMouseEvent;
 class QMenu;
@@ -15,38 +14,30 @@ class QGraphicsLineItem;
 class QColor;
 //class QListView;
 QT_END_NAMESPACE
-
 class DiagramScene : public QGraphicsScene
 {
-    Q_OBJECT
-
+Q_OBJECT
 public:
-    enum Mode { InsertItem, InsertLine, MoveItem };
-
-    explicit DiagramScene(ModulesListModel * modulesListModel, QListView * modulesView, QMenu * itemMenu, QObject * parent = 0);
-
+enum Mode { InsertItem, InsertLine, MoveItem };
+explicit DiagramScene(ModulesListModel *modulesListModel, QListView *modulesView, QMenu *itemMenu, QObject *parent = 0);
 public slots:
-    void setMode(Mode mode);
-
+void setMode(Mode mode);
 signals:
-    void itemInserted();
-    void itemSelected(QGraphicsItem * item);
-
+void itemInserted();
+void itemSelected(QGraphicsItem *item);
 protected:
-    void mousePressEvent(QGraphicsSceneMouseEvent * mouseEvent);
-    void mouseMoveEvent(QGraphicsSceneMouseEvent * mouseEvent);
-    void mouseReleaseEvent(QGraphicsSceneMouseEvent * mouseEvent);
-
+void mousePressEvent(QGraphicsSceneMouseEvent *mouseEvent);
+void mouseMoveEvent(QGraphicsSceneMouseEvent *mouseEvent);
+void mouseReleaseEvent(QGraphicsSceneMouseEvent *mouseEvent);
 private:
-    QMenu * myItemMenu;
-    QListView * modulesView;
-    ModulesListModel * modulesListModel;
-    Mode myMode;
-    QPointF startPoint;
-    QGraphicsLineItem * line;
-    QColor myTextColor;
-    QColor myItemColor;
-    QColor myLineColor;
+QMenu *myItemMenu;
+QListView *modulesView;
+ModulesListModel *modulesListModel;
+Mode myMode;
+QPointF startPoint;
+QGraphicsLineItem *line;
+QColor myTextColor;
+QColor myItemColor;
+QColor myLineColor;
 };
-
 #endif // DIAGRAMSCENE_H
