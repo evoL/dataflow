a585b58f69af7987f31b09a36b83465addaaf49d
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-11-30 23:52:24 +0100

    Create ModuleDescription.cpp

---
+++ gui/ModuleDescription.cpp
@@ -0,0 +1,39 @@
+#include "moduledescription.h"
+
+
+ModuleDescription::ModuleDescription(QString s, QGraphicsItem * parent) : QGraphicsTextItem(parent)
+{
+    text_description=s;
+    setPlainText(s);
+}
+ModuleDescription::ModuleDescription(QGraphicsItem * parent) : QGraphicsTextItem(parent)
+{
+    text_description="";
+
+}
+
+void ModuleDescription::SetDescription(const QString desc)
+{
+    setPlainText(desc);
+}
+
+bool ModuleDescription::Is_text_long()
+{
+    if(text_description.length()>30) return true;
+    else return false;
+}
+
+qreal ModuleDescription::Scale()
+{
+    qreal height=1;
+    qreal text_width;
+    text_width=text_description.length();
+
+    while(text_width>20)
+    {
+        text_width=text_width-20;
+        height=height+1;
+    }
+
+    return 10*height;
+}
