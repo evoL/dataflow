bd36a7af90ec418a3f0b2bc07e560d3336267415
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-11-30 23:38:36 +0100

    Update Arrow.h

--- gui/Arrow.h
+++ gui/Arrow.h
@@ -1,45 +1,38 @@
 #ifndef ARROW_H
 #define ARROW_H
-
 #include <QGraphicsLineItem>
 #include "DiagramModuleItem.h"
-
+#include "ModuleIn.h"
+#include "ModuleOut.h"
 QT_BEGIN_NAMESPACE
 class QGraphicsPolygonItem;
 class QGraphicsLineItem;
 class QGraphicsScene;
 class QRectF;
+class QPolygonF;
 class QGraphicsSceneMouseEvent;
 class QPainterPath;
 QT_END_NAMESPACE
-
 class DiagramModuleItem;
-
 class Arrow : public QGraphicsLineItem
 {
 public:
-    enum { Type = UserType + 4 };
-
-    Arrow(DiagramModuleItem * startItem, DiagramModuleItem * endItem,
-          QGraphicsItem * parent = 0);
-
-    int type() const { return Type; }
-    QRectF boundingRect() const;
-    QPainterPath shape() const;
-    void setColor(const QColor & color) { myColor = color; }
-    DiagramModuleItem * startItem() const { return myStartItem; }
-    DiagramModuleItem * endItem() const { return myEndItem; }
-
-    void updatePosition();
-
+enum { Type = UserType + 4 };
+Arrow(ModuleIn *startItem, ModuleOut *endItem,
+QGraphicsItem *parent = 0);
+int type() const { return Type; }
+QRectF boundingRect() const;
+QPainterPath shape() const;
+void setColor(const QColor &color) { myColor = color; }
+ModuleIn *startItem() const { return myStartItem; }
+ModuleOut *endItem() const { return myEndItem; }
+void updatePosition();
 protected:
-    void paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget = 0);
-
+void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget = 0);
 private:
-    DiagramModuleItem * myStartItem;
-    DiagramModuleItem * myEndItem;
-    QColor myColor;
-    QPolygonF arrowHead;
+ModuleIn *myStartItem;
+ModuleOut *myEndItem;
+QColor myColor;
+QPolygonF arrowHead;
 };
-
 #endif // ARROW_H
