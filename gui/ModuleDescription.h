e194da77f09715bfb721b0878c6ed9e8a9e1aafa
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-11-30 23:52:58 +0100

    Create ModuleDescription.h

---
+++ gui/ModuleDescription.h
@@ -0,0 +1,19 @@
+#ifndef MODULEDESCRIPTION_H
+#define MODULEDESCRIPTION_H
+
+#include <QGraphicsTextItem>
+#include <QString>
+
+class ModuleDescription : public QGraphicsTextItem
+{
+public:
+    ModuleDescription(QGraphicsItem * parent = 0);
+    ModuleDescription(QString s,QGraphicsItem * parent = 0);
+    void SetDescription(const QString desc);
+    qreal Scale();
+    bool Is_text_long();
+private:
+    QString text_description;
+};
+
+#endif // MODULEDESCRIPTION_H
