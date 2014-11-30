0347963157264cfe82575ebabdc7d4b6496c6edb
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-11-30 23:50:58 +0100

    Update ModulesListModel.h

--- gui/ModulesListModel.h
+++ gui/ModulesListModel.h
@@ -1,23 +1,19 @@
 #ifndef MODULESLISTMODEL_H
 #define MODULESLISTMODEL_H
-
 #include <QAbstractListModel>
 #include <QList>
 #include "DiagramModuleItem.h"
 #include "Module.h"
-
 class ModulesListModel : public QAbstractListModel
 {
 public:
-    ModulesListModel();
-    int rowCount(const QModelIndex & parent) const;
-    QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
-    void append(Module element);
-    const Module & at(int index);
-    void removeAt(int index);
-
+ModulesListModel();
+int rowCount(const QModelIndex & parent) const;
+QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+void append(Module element);
+const Module &at(int index);
+void removeAt(int index);
 private:
-    QList<Module> list;
+QList<Module> list;
 };
-
 #endif // MODULESLISTMODEL_H
