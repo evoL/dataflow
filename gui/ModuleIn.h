abba5b24382c3b4cef9a404a0990ae481161ac29
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-11-30 23:54:37 +0100

    Create ModuleIn.h

---
+++ gui/ModuleIn.h
@@ -0,0 +1,31 @@
+#ifndef MODULEIN_H
+#define MODULEIN_H
+
+#include <QGraphicsPolygonItem>
+#include <QStyleOptionGraphicsItem>
+#include <QList>
+#include <QPointF>
+
+class Arrow;
+class ModuleIn : public QGraphicsPolygonItem
+{
+public:
+
+    enum { Type = UserType + 1 };
+    ModuleIn(QGraphicsItem * parent=0);
+    void DrawIn(int i);
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+    void highlight (QPainter *painter,bool is_selected);
+    int type() const { return Type;}
+    void addArrow(Arrow *arrow);
+    void removeArrow(Arrow *arrow);
+    void removeArrows();
+    QPointF pos() const;
+
+private:
+    int posi;
+    QList<Arrow *> arrows;
+
+};
+
+#endif // MODULEIN_H
