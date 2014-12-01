5a86abd7adac04aadd75f04129db5c407596450e
Karol Wieczorek <karolwieczorek@MacBook-Air-Karol.local>
2014-12-01 23:42:56 +0100

    Updated model to suit our new needs
    - added enum BlockType
    - added virtual method BlockType to Block class and its descendants
    - used shared pointers instead of unique pointers

--- interpreter/DataflowModel.h
+++ interpreter/DataflowModel.h
@@ -5,9 +5,16 @@
 #include <unordered_map>
 #include <memory>
 
+#include "XMLParser.h"
+
 struct Block;
 
-typedef std::unordered_map<int, std::unique_ptr<Block>> BlocksMap;
+typedef std::unordered_map<int, std::shared_ptr<Block>> BlocksMap;
+
+typedef enum {
+    BlockTypeOperation,
+    BlockTypeConstructor
+} BlockType;
 
 struct Position {
     float x;
@@ -17,7 +24,7 @@ struct Position {
 struct InputTransition {
     int id;
     int output_id;
-    Block * outputBlock;
+    std::shared_ptr<Block> outputBlock;
 };
 
 struct OutputTransition {
@@ -29,6 +36,9 @@ struct Block {
     Block(int id, std::string module = "", Position position = Position {0.0, 0.0}, const std::list<OutputTransition> & outputs = std::list<OutputTransition>())
         : id(id), module(module), position(position), outputs(outputs) {}
 
+    // methods
+    virtual BlockType blockType() const = 0;
+    
     // fields
     int id;
     std::string module;
@@ -38,9 +48,12 @@ struct Block {
 
 struct Constructor : Block {
     // constructor
-    Constructor(int id, std::string module = "", Position position = Position {0.0, 0.0}, const std::list<OutputTransition> & outputs = std::list<OutputTransition>(), std::string type = "", const char * data = "")
+    Constructor(int id, std::string module = "", std::string type = "", Position position = Position {0.0, 0.0}, const std::list<OutputTransition> & outputs = std::list<OutputTransition>(), const char * data = "")
         : Block(id, module, position, outputs), type(type), data(data) {}
 
+    // methods
+    virtual BlockType blockType() const { return BlockTypeConstructor; }
+    
     // fields
     std::string type;
     const char * data;
@@ -48,9 +61,12 @@ struct Constructor : Block {
 
 struct Operation : Block {
     // constructor
-    Operation(int id, std::string module = "", Position position = Position {0.0, 0.0}, const std::list<OutputTransition> & outputs = std::list<OutputTransition>(), const std::list<InputTransition> inputs = std::list<InputTransition>(), std::string name = "")
+    Operation(int id, std::string module = "",  std::string name = "", Position position = Position {0.0, 0.0}, const std::list<OutputTransition> & outputs = std::list<OutputTransition>(), const std::list<InputTransition> inputs = std::list<InputTransition>())
         : Block(id, module, position, outputs), inputs(inputs), name(name) {}
 
+    // methods
+    virtual BlockType blockType() const { return BlockTypeOperation; }
+    
     // fields
     std::list<InputTransition> inputs;
     std::string name;
@@ -58,15 +74,20 @@ struct Operation : Block {
 
 class DataflowProjectModel
 {
+    friend class XMLParser;
+    
 public:
     // getters
     const std::string & getName() { return name; }
     const std::list<std::string> & getImports() { return imports; }
+    const std::list<int> & getEntryPoints() { return entryPoints; }
     const Block & getBlock(int id) { return *blocks[id]; }
 
-private:
+//private:
+public:
     std::string name;
     std::list<std::string> imports;
+    std::list<int> entryPoints;
     BlocksMap blocks;
 };
 
