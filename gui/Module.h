4cc6eee090669b1c296ad1c6a7bb4704af6a776c
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-12-07 18:02:38 +0100

    Module can be operation, input or output

--- gui/Module.h
+++ gui/Module.h
@@ -6,7 +6,7 @@
 class Module
 {
 public:
-    Module(QString name, QSize size, QVector<QString> id_in, QVector<QString> id_out);
+    Module(QString name, QSize size, QVector<QString> id_in, QVector<QString> id_out,int t=0);
     QString toString() const { return name; }
     QSize size() const { return blockSize; }
     void setName(QString newName) { name = newName; }
@@ -14,8 +14,10 @@ public:
     QVector<QString> In;
     QVector<QString> Out;
     QString name;
+    int getType();
 private:
     QSize blockSize;
+    int blockType; //-1=input block, 1=output block, 0=operation block
 
 };
 #endif // MODULE_H
