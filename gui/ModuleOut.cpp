b57ea994176fc830f675408fd71667abfe79f3c0
agnieszka-hejna <hejna.agnieszka@gmail.com>
2014-12-07 16:44:56 +0100

    Description of OUTs

--- gui/ModuleOut.cpp
+++ gui/ModuleOut.cpp
@@ -5,18 +5,33 @@
 #include <QPainter>
 #include <QGraphicsScene>
 #include <QGraphicsSceneContextMenuEvent>
+#include <QTextBlockFormat>
+#include <QTextCursor>
 
 ModuleOut::ModuleOut(QGraphicsItem * parent) : QGraphicsEllipseItem(parent)
 {
     setFlag(QGraphicsItem::ItemIsSelectable, true);
 }
 
-void ModuleOut::DrawOut(int i)
+void ModuleOut::DrawOut(int i, QString text)
 {
     posi = i;
     setBrush(Qt::red);
     qreal moduleNameRectHeight = qgraphicsitem_cast< DiagramModuleItem* >(parentItem())->moduleName->getBlockHeight();
     setRect( qgraphicsitem_cast< DiagramModuleItem* >(parentItem())->getWidth() - 8 , moduleNameRectHeight + 5 + posi*20, 15, 15);
+
+    QGraphicsTextItem *outDesc=new QGraphicsTextItem(this);
+    outDesc->setPlainText(text.mid(0,10));
+    outDesc->setX(qgraphicsitem_cast< DiagramModuleItem* >(parentItem())->getWidth() - 80);
+    outDesc->setY(3+posi*20+moduleNameRectHeight);
+    outDesc->setTextWidth(70);
+    QTextBlockFormat format;
+    format.setAlignment(Qt::AlignRight);
+    QTextCursor cursor = outDesc->textCursor();
+    cursor.select(QTextCursor::Document);
+    cursor.mergeBlockFormat(format);
+    cursor.clearSelection();
+    outDesc->setTextCursor(cursor);
 }
 
 void ModuleOut::paint(QPainter * painter, const QStyleOptionGraphicsItem * option, QWidget * widget)
@@ -66,3 +81,4 @@ QPointF ModuleOut::pos() const
     QPoint q(qgraphicsitem_cast< DiagramModuleItem* >(parentItem())->getWidth() , moduleNameRectHeight + 12 + 20 * posi);
     return parentItem()->pos() + q;
 }
+
