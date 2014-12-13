8ea638c989a82db0405e9bc9780d0167fcde3df1
Rafał Hirsz <rafal@hirsz.co>
2014-12-13 21:49:47 +0100

    Removed compilation error on MinGW.

--- interpreter/library_loading.cpp
+++ interpreter/library_loading.cpp
@@ -7,7 +7,7 @@ DATAFLOW_LIBRARY dataflow::library_load(const std::string & libraryName)
     return LoadLibrary((libraryName + ".dll").c_str());
 }
 
-void * dataflow::library_procedure(DATAFLOW_LIBRARY library, const std::string & name)
+DATAFLOW_PROCEDURE dataflow::library_procedure(DATAFLOW_LIBRARY library, const std::string & name)
 {
     return GetProcAddress(library, name.c_str());
 }
@@ -32,7 +32,7 @@ DATAFLOW_LIBRARY dataflow::library_load(const std::string & libraryName)
     return dlopen(("lib" + libraryName + DATAFLOW_LIBRARY_EXTENSION).c_str(), RTLD_LAZY);
 }
 
-void * dataflow::library_procedure(DATAFLOW_LIBRARY library, const std::string & name)
+DATAFLOW_PROCEDURE dataflow::library_procedure(DATAFLOW_LIBRARY library, const std::string & name)
 {
     return dlsym(library, name.c_str());
 }
--- interpreter/library_loading.h
+++ interpreter/library_loading.h
@@ -6,14 +6,16 @@
 #ifdef _WIN32
 #   include <Windows.h>
 #   define DATAFLOW_LIBRARY HMODULE
+#   define DATAFLOW_PROCEDURE FARPROC
 #else
 #   define DATAFLOW_LIBRARY void *
+#   define DATAFLOW_PROCEDURE void *
 #endif
 
 namespace dataflow
 {
     DATAFLOW_LIBRARY library_load(const std::string & libraryName);
-    void * library_procedure(DATAFLOW_LIBRARY library, const std::string & name);
+    DATAFLOW_PROCEDURE library_procedure(DATAFLOW_LIBRARY library, const std::string & name);
     bool   library_free(DATAFLOW_LIBRARY library);
 }
 
