fcbdf4f264d7abdd7f9af31fdffbe961f87741c6
Rafał Hirsz <rafal@hirsz.co>
2015-01-11 18:06:56 +0100

    Pimped up the interpreter example to support file names from ARGV.

--- interpreter/interpreter_example.cpp
+++ interpreter/interpreter_example.cpp
@@ -4,15 +4,25 @@
 #include "Interpreter.h"
 #include "ModelManipulator.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    XMLParser parser;
-    std::unique_ptr<ProjectModel> model{ parser.loadModelFromFile("C:\\Users\\Prymus\\Documents\\Uni\\Projekt zespolowy\\dataflow\\ppm_sample.xml") };
+    if (argc == 1) {
+        std::cerr << "Usage: " << argv[0] << " file.xml" << std::endl;
+        return 1;
+    }
 
-    ModelManipulator manipulator(*model);
+    try {
+        XMLParser parser;
+        std::unique_ptr<ProjectModel> model{ parser.loadModelFromFile(argv[1]) };
 
-    Interpreter interpreter(*model);
-    interpreter.interpret();
+        ModelManipulator manipulator(*model);
+
+        Interpreter interpreter(*model);
+        interpreter.interpret();
+    } catch (XMLParserError e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 }
 
 
