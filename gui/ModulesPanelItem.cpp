80301ea2485257b863caa1e4b0bde9f41d5a1746
Rafał Hirsz <rafal@hirsz.co>
2015-01-18 01:40:47 +0100

    Silenced some warnings.

--- gui/ModulesPanelItem.cpp
+++ gui/ModulesPanelItem.cpp
@@ -2,10 +2,10 @@
 
 ModulesPanelItem::ModulesPanelItem(const std::string name, const void * dataPtr, ItemType itemType, ModulesPanelItem * parent)
 {
-	this->name = name;
-	this->itemType = itemType;
-	this->dataPtr = dataPtr;
-	this->parentItem = parent;
+    this->name = name;
+    this->itemType = itemType;
+    this->dataPtr = dataPtr;
+    this->parentItem = parent;
 }
 
 ModulesPanelItem::~ModulesPanelItem()
@@ -14,87 +14,89 @@ ModulesPanelItem::~ModulesPanelItem()
 
 ModulesPanelItem * ModulesPanelItem::child(int row)
 {
-	switch (itemType)
-	{
-	case RootT:
-	case LibraryT:
-	case OperationsLabelT:
-	case ConstructorsLabelT:
-		if (row >= 0 && row < children.size())
-			return children.at(row);
-	}
-	return NULL;
+    switch (itemType)
+    {
+    case RootT:
+    case LibraryT:
+    case OperationsLabelT:
+    case ConstructorsLabelT:
+        if (row >= 0 && row < children.size())
+            return children.at(row);
+    default:
+        return NULL;
+    }
 }
 
 ModulesPanelItem * ModulesPanelItem::parent()
 {
-	return parentItem;
+    return parentItem;
 }
 
 int ModulesPanelItem::row() const
 {
-	if (parentItem)
-		return parentItem->children.indexOf(const_cast<ModulesPanelItem*>(this));
+    if (parentItem)
+        return parentItem->children.indexOf(const_cast<ModulesPanelItem*>(this));
 
-	return 0;
+    return 0;
 }
 
 int ModulesPanelItem::childCount()
 {
-	return children.count();
+    return children.count();
 }
 
 int ModulesPanelItem::columnCount()
 {
-	return 1;
+    return 1;
 }
 
 QVariant ModulesPanelItem::data()
 {
-	switch (itemType)
-	{
-	case LibraryT:
-	case OperationsLabelT:
-	case OperationT:
-	case ConstructorsLabelT:
-	case ConstructorT:
-		return QString::fromStdString(name);
-	}
-	return QString("No data");
+    switch (itemType)
+    {
+    case LibraryT:
+    case OperationsLabelT:
+    case OperationT:
+    case ConstructorsLabelT:
+    case ConstructorT:
+        return QString::fromStdString(name);
+    default:
+        return QString("No data");
+    }
 }
 
 ModulesPanelItem * ModulesPanelItem::addLibrary(const Library * library)
 {
-	ModulesPanelItem * newLibrary = new ModulesPanelItem(library->getNameToPrint(), library, LibraryT, this);
-	children.push_back(newLibrary);
-	return newLibrary;
+    ModulesPanelItem * newLibrary = new ModulesPanelItem(library->getNameToPrint(), library, LibraryT, this);
+    children.push_back(newLibrary);
+    return newLibrary;
 }
 
 ModulesPanelItem * ModulesPanelItem::addOperationsLabel()
 {
-	ModulesPanelItem * newLabel = new ModulesPanelItem("Operations", NULL, OperationsLabelT, this);
-	children.push_back(newLabel);
-	return newLabel;
+    ModulesPanelItem * newLabel = new ModulesPanelItem("Operations", NULL, OperationsLabelT, this);
+    children.push_back(newLabel);
+    return newLabel;
 }
 
 ModulesPanelItem * ModulesPanelItem::addConstructorsLabel()
 {
-	ModulesPanelItem * newLabel = new ModulesPanelItem("Constructors", NULL, ConstructorsLabelT, this);
-	children.push_back(newLabel);
-	return newLabel;
+    ModulesPanelItem * newLabel = new ModulesPanelItem("Constructors", NULL, ConstructorsLabelT, this);
+    children.push_back(newLabel);
+    return newLabel;
 }
 
 ModulesPanelItem * ModulesPanelItem::addOperation(const std::string operation)
 {
-	// Have to consider if we want to keep dataPtr field in this class. Now we don't use it and pass NULL now
-	ModulesPanelItem * newOperation = new ModulesPanelItem(operation, NULL, OperationT, this);
-	children.push_back(newOperation);
-	return newOperation;
+    // Have to consider if we want to keep dataPtr field in this class. Now we don't use it and pass NULL now
+    ModulesPanelItem * newOperation = new ModulesPanelItem(operation, NULL, OperationT, this);
+    children.push_back(newOperation);
+    return newOperation;
 }
 
 ModulesPanelItem * ModulesPanelItem::addConstructor(const std::string constructor)
 {
-	ModulesPanelItem * newConstructor = new ModulesPanelItem(constructor, NULL, ConstructorT, this);
-	children.push_back(newConstructor);
-	return newConstructor;
+    ModulesPanelItem * newConstructor = new ModulesPanelItem(constructor, NULL, ConstructorT, this);
+    children.push_back(newConstructor);
+    return newConstructor;
 }
